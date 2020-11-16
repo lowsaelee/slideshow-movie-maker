c0683ee53336fcfed2c149350db98adc6734fd23
lowsaelee <62345094+lowsaelee@users.noreply.github.com>
2020-11-15 21:10:35 -0800

    Add files via upload

--- slideshowMaker.py
+++ slideshowMaker.py
@@ -4,6 +4,10 @@ Created on Tue Oct 20 08:10:01 2020
 
 Slideshow movie maker with audio
 
+Creates a batch files for running with ffmpg
+
+Limit with length of video - too many arguments and ffmpeg will not read it
+
 @author: Low
 """
 
@@ -22,27 +26,25 @@ Slideshow movie maker with audio
 import os
 import random
 
-#sanchinVideoDuration = [14,12.85,12.21,10.23,13.1,9.88,12.25,8.86,11.99,9.91,11.62,8,11.97,9.33,10.54,7.81,10.12,8.69,9.37,9.72,10.57,8.57,9.63,8.91,9.95,8.88,10.96,8.73,11.43,9.07,10.88,8.12,11.03,8.04,9.91,8.16,10.18,8.54,11.83,9.66,8.6,7.66,10.27,8.69,9.49,8.45,9.15,9.27,7.41,9.63,10.52,9.33,10.12,8.74,7.15,7.96,9.83,8.72,7.16,8.56,7.98,8.89,9.11,8.45,8.84,11.94,9.63,10.57,9.79,8.51,8.75,9.25,9.73,10.37,9.43,8.61,10.88,8.77,9.65,9.49,11.23,8.85,9.35,10.16,9.13,8.92,9.7,10.36,9.05,11.59,9.38,8.36,9.41,9.94,9.75,8.48,9.16,10.5,10.28,10.29,9.4,9.84,11.02,9.39,8.07,9.98,9.7,11.54,9.13,8.84,9.95,11.88,7.17,8.68,9.28,10.57,7.96,9.51,10.09,9.32,9.86,9.47,10.62,8.07,9.69,7.8,10.16,10.3,8.1,7.91,8.63,8.59,8.92,7.91,10.07,8.29,10.07,8.98,9.75,8.26,9.6,8.64,9.99,9.5,10.05,6.71,9.3,8.35,11.12,9.23,10.7,10.52,9,5.74,7.43,8.41,8.57,8.81,7.35,9.46,9.52,8.12,9.54,9.09,6.43,8.18,8.72,]
+#VariableVideoDuration = [14,12.85,12.21,10.23,13.1,9.88,12.25,8.86,11.99,9.91,11.62,8,11.97,9.33,10.54,7.81,10.12,8.69,9.37,9.72,10.57,8.57,9.63,8.91,9.95,8.88,10.96,8.73,11.43,9.07,10.88,8.12,11.03,8.04,9.91,8.16,10.18,8.54,11.83,9.66,8.6,7.66,10.27,8.69,9.49,8.45,9.15,9.27,7.41,9.63,10.52,9.33,10.12,8.74,7.15,7.96,9.83,8.72,7.16,8.56,7.98,8.89,9.11,8.45,8.84,11.94,9.63,10.57,9.79,8.51,8.75,9.25,9.73,10.37,9.43,8.61,10.88,8.77,9.65,9.49,11.23,8.85,9.35,10.16,9.13,8.92,9.7,10.36,9.05,11.59,9.38,8.36,9.41,9.94,9.75,8.48,9.16,10.5,10.28,10.29,9.4,9.84,11.02,9.39,8.07,9.98,9.7,11.54,9.13,8.84,9.95,11.88,7.17,8.68,9.28,10.57,7.96,9.51,10.09,9.32,9.86,9.47,10.62,8.07,9.69,7.8,10.16,10.3,8.1,7.91,8.63,8.59,8.92,7.91,10.07,8.29,10.07,8.98,9.75,8.26,9.6,8.64,9.99,9.5,10.05,6.71,9.3,8.35,11.12,9.23,10.7,10.52,9,5.74,7.43,8.41,8.57,8.81,7.35,9.46,9.52,8.12,9.54,9.09,6.43,8.18,8.72,]
 
 VariableVideoDuration = [14,12.85,12.21,10.23,13.1,9.88,12.25,8.86,11.99,9.91,11.62,8,11.97,9.33,10.54,7.81,10.12,8.69,9.37,9.72,10.57]
 
 
-pictureDir = "D:\Data\Docs\pythonprojects\SlideshowMaker\pictures"
+
+pictureDir = "C:\Pictures"
 realLength = 0 ## length of actual list size instead of default 300
 durationPerImage = 5
 #need to program this
-audioMinutes = 4
-audioSeconds = 11
+audioMinutes = 27
+audioSeconds = 34
 audioDuration = (audioMinutes * 60) + audioSeconds + durationPerImage #seconds (2 minutes 5 seconds)
 videoDuration = 0
 randomizeImages = True
 verticalVideo = False
 paddingColor = "004D33"
-crossfade = True
+crossfade = False
 variableDuration = False
-
-#special
-#audioDuration = sum(sanchinVideoDuration)+(len(sanchinVideoDuration)*2)
 
 print("actual length = ",sum(VariableVideoDuration))
 print("added artificial length = ",audioDuration)
@@ -115,15 +117,15 @@ if (emptyDirectory == False):
         #if imageFiles[i] != 0:
         if verticalVideo == False:
             if crossfade == False:
-                ffmpegCommand = ffmpegCommand + "[" + str(i) + ":v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2:#" + paddingColor + "@1,format=rgb24,setsar=1,fade=t=in:st=0:d=1,fade=t=out:st=" + str(durationPerImage) + ":d=1[v" + str(i) + "]; "
+                ffmpegCommand = ffmpegCommand + "[" + str(i) + ":v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2:#" + paddingColor + "@1,format=rgb24,setsar=1,fade=t=in:st=0:d=1,fade=t=out:st=" + str(durationPerImage) + ":d=1[v" + str(i) + "]; "
             else:
                 ffmpegCommand = ffmpegCommand + "[" + str(i+1) + "]format=rgb24,fade=d=1:t=in:alpha=1,setpts=PTS-STARTPTS+" + str((i+1)*(durationPerImage-1)) + "/TB[f" + str(i) + "]; "
         if verticalVideo == True:
             if crossfade == False:
-                ffmpegCommand = ffmpegCommand + "[" + str(i) + ":v]scale=1080:1920:force_original_aspect_ratio=decrease,pad=720:1280:(ow-iw)/2:(oh-ih)/2:#" + paddingColor + "@1,format=rgb24,setsar=1,fade=t=in:st=0:d=1,fade=t=out:st=" + str(durationPerImage) + ":d=1[v" + str(i) + "]; "
+                ffmpegCommand = ffmpegCommand + "[" + str(i) + ":v]scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2:#" + paddingColor + "@1,format=rgb24,setsar=1,fade=t=in:st=0:d=1,fade=t=out:st=" + str(durationPerImage) + ":d=1[v" + str(i) + "]; "
             else:
                 if variableDuration == True:
-                    durationPerImage = sanchinVideoDuration[i]
+                    durationPerImage = VariableVideoDuration[i]
                 ffmpegCommand = ffmpegCommand + "[" + str(i+1) + "]format=rgb24,fade=d=1:t=in:alpha=1,setpts=PTS-STARTPTS+" + str((i+1)*(durationPerImage-1)) + "/TB[f" + str(i) + "]; "
 
    
@@ -154,8 +156,6 @@ if (emptyDirectory == False):
     outputFile.write(ffmpegCommand)
     outputFile.close() 
     
-    #os.system('cmd /c "D:/Docs/pythonprojects/SlideshowMaker/slideshow.bat"')
-
 
 """
     # includes command for encoding video with audio
